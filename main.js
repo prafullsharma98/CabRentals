e900ed9664090ac8a3cb908b8dbe28801ac9b9e3
tanujmody <tanujmody@users.noreply.github.com>
2018-10-24 10:50:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function myFunction() {
+   var from= document.getElementById("fromDate").value;
+   var to= document.getElementById("toDate").value;
+   if(to<from)
+   {alert("error")
+   }
+}
+
+
