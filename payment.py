5406696d4da926800d29584ededb15e91e644e9f
rajdadia <rajdadia@gmail.com>
2018-11-03 01:51:18 +0530

    neglect

--- payment.py
+++ payment.py
@@ -1,4 +1,4 @@
-#!"C:\Users\Mahe\Anaconda3\python.exe"
+#!"E:\Python3\python.exe"
 # -*- coding: UTF-8 -*-
 
 # Import modules for CGI handling 
@@ -9,36 +9,37 @@ import sqlite3
 form = cgi.FieldStorage() 
 
 # Get data from fields
-carid  = form.getvalue('carids')
-name  = form.getvalue('name')
-emailid  = form.getvalue('emailid')
-fromdate  = form.getvalue('fdate')
-todate = form.getvalue('tdate')
-days  = form.getvalue('days')
-adddress  = form.getvalue('address')
-state  = form.getvalue('state')
-ptime = form.getvalue('ptime')
-dtime = form.getvalue('dtime')
+carid  ="3"#form.getvalue('carid')
+name  ="12" #form.getvalue('name')
+emailid  = "2"#form.getvalue('emailid')
+fromdate  ="2" #form.getvalue('fdate')
+todate ="2" #form.getvalue('tdate')
+days  = "1"#form.getvalue('days')
+adddress  ="23" #form.getvalue('address')
+state  ="3" #form.getvalue('state')
+ptime ="1" #form.getvalue('ptime')
+dtime ="2" #form.getvalue('dtime')
 
 
 conn = sqlite3.connect('customer.db')
 
-cursor = conn.execute("SELECT PRICEPERDAY FROM CAR WHERE ID=?",(carid,))
+cursor = conn.execute("SELECT PRICEPERDAY FROM CAR WHERE ID=?",(carid))
 for row in cursor:
 	price=str(row[0])
 
-print("Content-type:text/html\r\n\r\n")
-print("<html>")
-print("<head>")
-print("<link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>")
-print("<title>payment stats</title>")
-print("</head>")
-print("<body>")
-print("hello")
+#print("Content-type:text/html\r\n\r\n")
+# print("<html>")
+# print("<head>")
+# print("<link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>")
+# print("<title>payment stats</title>")
+# print("</head>")
+# print("<body>")
+# print("hello")
+print(price)
 
 pay=str(int(price)*int(days))
 
-print("<p> %s </p>" % (pay,))
+# print("<p> %s </p>" % (pay))
 
 carid  = str(carid)
 name  = str(name)
@@ -54,18 +55,18 @@ pay=str(pay)
 
 
 
-print("<p> %s %s %s %s %s %s %s %s %s %s %s</p>" % (name,emailid,carid,fromdate,todate,days,address,state,ptime,dtime,pay))
+# print("<p> %s %s %s %s %s %s %s %s %s %s %s</p>" % (name,emailid,carid,fromdate,todate,days,address,state,ptime,dtime,pay))
 
 
-# conn.execute("INSERT INTO PAYMENT(NAME,EMAILID,CARID,FROMDATE,TODATE,DAYS,ADDRESS,STATE,PICKUPTIME,DROPOFFTIME,PAY) VALUES (?,?,?,?,?,?,?,?,?,?,?)",(name,emailid,carid,fromdate,todate,days,address,state,ptime,dtime,pay))
+conn.execute("INSERT INTO PAYMENT(NAME,EMAILID,CARID,FROMDATE,TODATE,DAYS,ADDRESS,STATE,PICKUPTIME,DROPOFFTIME,PAY) VALUES (?,?,?,?,?,?,?,?,?,?,?)",(name,emailid,carid,fromdate,todate,days,address,state,ptime,dtime,pay))
 
-# conn.commit()
+conn.commit()
 
-print("<p>rec added</p>")
+# print("<p>rec added</p>")
 
 conn.close()
 # print("""<a href="https://www.google.com" id="za"> Click me </a>""")
 # print("""<script type="text/javascript"> document.getElementById('za').click() </script>""")
 
-print("</body>")
-print("</html>")
+# print("</body>")
+# print("</html>")
