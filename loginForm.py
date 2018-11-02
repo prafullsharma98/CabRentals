4dbfdd6c8928cb2d4c61942ec55371ddb04aa298
Tarun Rath <39010622+TarunRath@users.noreply.github.com>
2018-11-03 02:06:13 +0530

    Add files via upload

--- loginForm.py
+++ loginForm.py
@@ -4,6 +4,7 @@
 # Import modules for CGI handling 
 import cgi, cgitb 
 import sqlite3
+import time
 
 #conn = sqlite3.connect('name.db')
 
@@ -22,7 +23,9 @@ conn = sqlite3.connect('customer.db')
 print("Content-type:text/html\r\n\r\n")
 print("<html>")
 print("<head>")
+print("<link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>")
 print("<title>Hello - Stats</title>")
+print("<style type='text/css'>img{ margin-left: 570px; margin-top: 100px;}</style>")
 print("</head>")
 print("<body>")
 
@@ -35,14 +38,28 @@ for row in cursor:
 	i+=1	
 
 if i==0 :
-	print("<h2>No such name<h2>")
+	print("<img  src='https://cdn-images-1.medium.com/max/1600/1*v9BSJbg-nwkz4g2VySImBA.png'><br>")
+	print("<h2 style='text-align: center;font-size: 40px'>Invalid User</h2><br>")
+	print("<div style='text-align:center'>")
+	print("<a href='http://localhost/login.html' style='font-size: 20px'> try again </a>")
 
 else :
-	if flag==True:
-		print("<h2>login successful</h2>") 
+	if flag==False:
+		print("<img  src='https://cdn-images-1.medium.com/max/1600/1*v9BSJbg-nwkz4g2VySImBA.png'><br>")
+		print("<h2 style='text-align: center;font-size: 40px'>login unsuccessful</h2><br>")
+		print("<div style='text-align:center'>")
+		print("<a href='http://localhost/login.html' style='font-size: 20px'> try again </a>")
 	else:
-		print("<h2>login unsuccessful</h2>")	  
+		cursor = conn.execute("SELECT FIRSTNAME from CUSTOMER where EMAILID=?",(emailid,))
+		for row in cursor:
+			n=str(row[0])
 
+		print("<a href='http://localhost/useroptions.html' id='za' hidden>go</a>")
+		print("<script type='text/javascript'>")
+		print("sessionStorage.setItem('id','"+emailid+"');sessionStorage.setItem('name','"+n+"');document.getElementById('za').click();")
+		print("</script>")
+
+		
 
 
 conn.close()
